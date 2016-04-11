b85ee79acfaff147c7ed70601d9ee0a4555b071d
Duq1ang <du1014316228@gmail.com>
2016-04-11 20:16:11 +0800

    Simplify the code

--- 118. Pascal's Triangle.h
+++ 118. Pascal's Triangle.h
@@ -5,25 +5,14 @@ class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
     vector<vector<int>>result;
-    if(numRows==0)
-        return result;
-    result.push_back(vector<int>());
-    result[0].push_back(1);
-    if(numRows==1)
-        return result;
-    result.push_back(vector<int>());
-    result[1].push_back(1);
-    result[1].push_back(1);
-    if(numRows==2)
-        return result;
-    for(int i=2;i<numRows;i++){
+    for(int i=0;i<numRows;i++){
         result.push_back(vector<int>());
-        result[i].push_back(1);
-        for(int j=1;j<i;j++){
-            int cur=result[i-1][j-1]+result[i-1][j];
-            result[i].push_back(cur);
+        for(int j=0;j<=i;j++){
+            if(j==0||j==i)
+               result[i].push_back(1);
+            else
+               result[i].push_back(result[i-1][j-1]+result[i-1][j]);
         }
-        result[i].push_back(1);
     }
     return result;
     }
